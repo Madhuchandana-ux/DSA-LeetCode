71e750650f0cb6f4e824dfb924e0cb503d86e221
Madhuchandana-ux <madhusunkara2006@gmail.com>
2026-08-30 13:43:10 +0530

    Time: 1 ms (54.35%) | Memory: 12.7 MB (62.31%) - LeetSync

---
+++ sql/496-next-greater-element-i/next-greater-element-i.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int, int> next_greater;
+        stack<int> st;
+
+        for (int num : nums2) {
+            while (!st.empty() && st.top() < num) {
+                next_greater[st.top()] = num;
+                st.pop();
+            }
+            st.push(num);
+        }
+
+        while (!st.empty()) {
+            next_greater[st.top()] = -1;
+            st.pop();
+        }
+
+        vector<int> ans;
+        for (int num : nums1) {
+            ans.push_back(next_greater[num]);
+        }
+
+        return ans;
+    }
+};
